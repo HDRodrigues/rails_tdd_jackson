e5d86677676819f72aff5b56aeff01a58f7334c2
Helton <helton.dias.rodrigues@gmail.com>
2018-01-07 18:15:57 -0200

    VCR configurado para utilizar somente assinatura

--- teste/spec/httparty/httparty_spec.rb
+++ teste/spec/httparty/httparty_spec.rb
@@ -1,14 +1,20 @@
 describe 'HTTParty' do
-    it '#content_type' do
+    # it '#content_type' do
         #mockando request com WEBMOCK
         # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
         #  to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
 
-        #UTILIZANDO VCR para mockar os dados de json
-        VCR.use_cassette("jsonplaceholder/posts") do
+        #UTILIZANDO VCR para mockar os dados de json - sem a integracao rspec na config do VCR
+        # VCR.use_cassette("jsonplaceholder/posts") do
+        #     response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+        #     content_type = response.headers['content-type']
+        #     expect(content_type).to match(/application\/json/)
+        # end
+    #end
+
+    it 'content_type', vcr: { cassette_name: 'jsonplaceholder/posts'} do
             response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
             content_type = response.headers['content-type']
             expect(content_type).to match(/application\/json/)
-        end
     end 
 end
