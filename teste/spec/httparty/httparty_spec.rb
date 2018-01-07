4c9499aacf245326f6628728811f9f0d1b7a851a
Helton <helton.dias.rodrigues@gmail.com>
2018-01-07 18:50:07 -0200

    VCR com URIs nao deterministicas

--- teste/spec/httparty/httparty_spec.rb
+++ teste/spec/httparty/httparty_spec.rb
@@ -12,8 +12,8 @@ describe 'HTTParty' do
         # end
     #end
 
-    it 'content_type', vcr: { cassette_name: 'jsonplaceholder/posts'} do
-            response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+    it 'content_type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
+            response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
             content_type = response.headers['content-type']
             expect(content_type).to match(/application\/json/)
     end 
