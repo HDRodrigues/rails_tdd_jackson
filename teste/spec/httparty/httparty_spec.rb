75af3d8a6336bf0a192a292285efececcd2368ab
Helton <helton.dias.rodrigues@gmail.com>
2018-01-07 18:58:39 -0200

    Modos de gravacao do VCR

--- teste/spec/httparty/httparty_spec.rb
+++ teste/spec/httparty/httparty_spec.rb
@@ -12,9 +12,15 @@ describe 'HTTParty' do
         # end
     #end
 
-    it 'content_type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
-            response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
-            content_type = response.headers['content-type']
-            expect(content_type).to match(/application\/json/)
-    end 
+    # it 'content_type', vcr: { cassette_name: 'jsonplaceholder/posts', match_requests_on: [:body]} do
+    #         response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
+    #         content_type = response.headers['content-type']
+    #         expect(content_type).to match(/application\/json/)
+    # end 
+
+    it 'content_type', vcr: { cassette_name: 'jsonplaceholder/posts', :record => :new_episodes} do
+        response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/4')
+        content_type = response.headers['content-type']
+        expect(content_type).to match(/application\/json/)
+end 
 end
