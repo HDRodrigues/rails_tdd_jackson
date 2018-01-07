91c98af9f20d2ea0429f19536cd42492963fdc65
Helton <helton.dias.rodrigues@gmail.com>
2018-01-07 17:35:03 -0200

    VCR mockando dados de api

--- teste/spec/httparty/httparty_spec.rb
+++ teste/spec/httparty/httparty_spec.rb
@@ -1,11 +1,14 @@
 describe 'HTTParty' do
     it '#content_type' do
         #mockando request com WEBMOCK
-        stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
-         to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
+        # stub_request(:get, "https://jsonplaceholder.typicode.com/posts/2").
+        #  to_return(status: 200, body: "", headers: {'content-type': 'application/json'})
 
-        response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
-        content_type = response.headers['content-type']
-        expect(content_type).to match(/application\/json/)
+        #UTILIZANDO VCR para mockar os dados de json
+        VCR.use_cassette("jsonplaceholder/posts") do
+            response = HTTParty.get('https://jsonplaceholder.typicode.com/posts/2')
+            content_type = response.headers['content-type']
+            expect(content_type).to match(/application\/json/)
+        end
     end 
 end
