f07c9481932dca1832b7a6f5a1d7e02b25241d44
Helton <helton.dias.rodrigues@gmail.com>
2018-01-07 16:04:17 -0200

    alternativa para sobrescrever factory em objeto referenciado

--- teste/spec/factories/orders.rb
+++ teste/spec/factories/orders.rb
@@ -2,6 +2,7 @@
 FactoryBot.define do
   factory :order do
     sequence(:description) {|n| "Pedido numero - #{n} "}
+    #association :customer, factory: :customer #Caso precise sobrescrever a fabrica
     customer
   end
 end
