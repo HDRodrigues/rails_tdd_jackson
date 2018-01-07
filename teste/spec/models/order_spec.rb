1f591ffb150f4b7306be52d3ccc4b3f9c9f598a2
Helton <helton.dias.rodrigues@gmail.com>
2018-01-07 16:23:33 -0200

    Testando has_many with traits

--- teste/spec/models/order_spec.rb
+++ teste/spec/models/order_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  it 'Tem 1 pedido' do
+  it 'belongs_to' do
     order = create(:order)
     puts order.description
     puts order.customer
@@ -12,5 +12,11 @@ RSpec.describe Order, type: :model do
   it 'Tem 3 pedidos' do
     orders = create_list(:order,3)
     expect(orders.count).to eq(3)
+  end
+  it 'has_many' do
+    # customer =create(:customer, :with_orders) #Utilizando o default da factory
+    customer =create(:customer_with_orders) #Utilizando factory with traits
+    #customer =create(:customer, :with_orders, qtt_orders: 5) #Sobrescrevendo
+    expect(customer.orders.count).to eq(3)
   end
 end
