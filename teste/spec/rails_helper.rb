b8dc60156d44db918ebbd0f95552d415a61bf313
Helton <helton.dias.rodrigues@gmail.com>
2018-01-14 11:14:10 -0200

    Utilizacao da gem shoulda matchers

--- teste/spec/rails_helper.rb
+++ teste/spec/rails_helper.rb
@@ -27,6 +27,16 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  #Shoulda Matchers
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      # Choose a test framework:
+      with.test_framework :rspec
+  
+      # Choose one or more libraries:
+      with.library :rails
+    end
+  end
 
   config.include ActiveSupport::Testing::TimeHelpers
 
