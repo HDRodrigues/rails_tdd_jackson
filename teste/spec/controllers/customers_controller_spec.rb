ba220c4cdc51f90db6c08f0b9b6381620a92cfb5
Helton <helton.dias.rodrigues@gmail.com>
2018-01-16 22:48:20 -0200

    Testando rotas utilizando shoulda matchers

--- teste/spec/controllers/customers_controller_spec.rb
+++ teste/spec/controllers/customers_controller_spec.rb
@@ -21,6 +21,10 @@ RSpec.describe CustomersController, type: :controller do
                 @member = create(:member)
                 @customer = create(:customer)
             end
+
+            it 'Route' do
+                (should route(:get, '/customers').to(action: :index))
+            end
             
             it 'Flash Notice' do
                 customer_params = attributes_for(:customer)
@@ -36,6 +40,14 @@ RSpec.describe CustomersController, type: :controller do
                 expect{
                     post :create, params:{customer:customer_params}
                 }.to change(Customer,:count).by(1)
+            end
+
+            it 'with invalid attributes' do
+                customer_params = attributes_for(:customer, address:nil)
+                sign_in @member
+                expect{
+                    post :create, params:{customer:customer_params}
+                }.not_to change(Customer,:count)
             end
 
             it '#show - 302 (not authorized)' do
